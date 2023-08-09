499617cab0bc1b349d3e49bb7277094a1ead1dfa
Nir_Linux <nl1984@gmail.com>
2023-08-09 19:09:42 +0300

    working with current date :)

--- chat3.py
+++ chat3.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, request, render_template
+from datetime import datetime  
+import calendar
+import time
 
 app = Flask(__name__)
 
@@ -14,7 +17,8 @@ def get_chat(room):
 
 @app.route('/api/chat/<room>', methods=['POST'])
 def post_message(room):
-    message = f"{request.form['username']}: {request.form['msg']}\n"
+    str_date_time = get_timestamp()
+    message = f"{str_date_time} || username:{request.form['username']} --> massage:{request.form['msg']}\n"
     filename = f'{room}.txt'
     with open(filename, 'a') as file:
         file.write(message)
@@ -27,6 +31,17 @@ def chat_room(room):
 @app.route('/')
 def index():
     return chat_room('general')  # Default to 'general' room
+
+
+def get_timestamp():
+
+    current_GMT = time.gmtime()
+    time_stamp = calendar.timegm(current_GMT)
+    from datetime import datetime  
+    date_time = datetime.fromtimestamp(time_stamp)
+    str_date_time = date_time.strftime("%d-%m-%Y %H:%M:%S")
+    return str_date_time
+
 
 if __name__ == '__main__':
     app.run(debug=True)
